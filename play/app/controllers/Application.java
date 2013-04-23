cd29519d53bd299d0f835d278cdb44d0aec15558
Viddu Devigere <viddu.devigere@macys.com>
2013-04-23 11:13:56 -0700

    Removing seed route, Converting to async Result

--- play/app/controllers/Application.java
+++ play/app/controllers/Application.java
@@ -4,7 +4,6 @@ import static play.data.Form.form;
 import static play.libs.Jsonp.jsonp;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,15 +18,15 @@ import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.JsonMappingException;
 
 import play.data.Form;
+import play.libs.F.Function;
 import play.libs.F.Promise;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Application extends Controller {
-    private static TestDataForm testData;
+   // private static TestDataForm testData;
     private static final Random random = new Random();
-
 
     /**
      * The main functionality that sets the cookie in a response.
@@ -37,12 +36,16 @@ public class Application extends Controller {
      * @throws JsonMappingException
      * @throws JsonGenerationException
      */
-    public static Result index(String callback) throws JsonGenerationException, JsonMappingException, IOException {
-        if (testData == null) {
-            Promise<TestDataForm> promiseOfTestData = RedisPersister.getPromiseOfTestData();
-            Application.testData = promiseOfTestData.get();
-        }
-        return generateRandomVariantResult(testData, callback);
+    public static Result index(final String callback) throws JsonGenerationException, JsonMappingException, IOException {
+          Promise<TestDataForm> promiseOfTestData = RedisPersister.getPromiseOfTestData();
+          return async(promiseOfTestData.map(new Function<TestDataForm, Result>() {
+
+              @Override
+              public Result apply(TestDataForm testData) throws Throwable {
+                  return generateRandomVariantResult(testData,callback);
+              }
+          }));
+          
     }
 
     /**
@@ -53,14 +56,17 @@ public class Application extends Controller {
      * @throws JsonMappingException
      * @throws JsonGenerationException
      */
-    public static Result allTests(String callback) throws JsonGenerationException, JsonMappingException, IOException {
-        if (testData == null) {
-            Promise<TestDataForm> promiseOfTestData = RedisPersister.getPromiseOfTestData();
-            Application.testData = promiseOfTestData.get();
-        }
-        Json json = new Json();
-        JsonNode jsonNode = json.toJson(testData);
-        return ok(jsonp(callback, jsonNode)).as("application/json");
+    public static Result allTests(final String callback) throws JsonGenerationException, JsonMappingException, IOException {
+        Promise<TestDataForm> promiseOfTestData = RedisPersister.getPromiseOfTestData();
+        return async(promiseOfTestData.map(new Function<TestDataForm, Result>() {
+
+            @Override
+            public Result apply(TestDataForm testData) throws Throwable {
+                Json json = new Json();
+                JsonNode jsonNode = json.toJson(testData);
+                return ok(jsonp(callback, jsonNode)).as("application/json");
+            }
+        }));
     }
 
     /**
@@ -70,39 +76,25 @@ public class Application extends Controller {
      */
     public static Result saveTestData() {
         Form<TestDataForm> testDataForm = form(TestDataForm.class).bindFromRequest();
-        testData = testDataForm.get();
-        Promise<Boolean> promiseOfStore = RedisPersister.getPromiseOfStoreTestData(testData);
-        //return ok(promiseOfStore.get().toString());
+        Promise<Boolean> promiseOfStore = RedisPersister.getPromiseOfStoreTestData(testDataForm.get());
         return redirect("/gui/test.html");
-    }
-
-    public static Result seedTestData() {
-        List<TestGroup> testGroupList = new ArrayList<TestGroup>();
-        Map<String, String> variantMap = new HashMap<String, String>();
-        variantMap.put("Experience A: Default", "70");
-        variantMap.put("Experience B: Alternate", "30");
-        TestGroup testGroup = new TestGroup("Header Flyouts Test", variantMap);
-        testGroupList.add(testGroup);
-        Application.testData = new TestDataForm(testGroupList);
-        Promise<Boolean> promiseOfStore = RedisPersister.getPromiseOfStoreTestData(testData);
-        return ok(promiseOfStore.get().toString());
     }
 
     private static Result generateRandomVariantResult(TestDataForm testData, String callback) throws IOException,
             JsonGenerationException, JsonMappingException {
         Map<String, String> randomVariants = new HashMap<String, String>();
         for (TestGroup testGroup : testData.getTestGroupList()) {
-            randomVariants.put(testGroup.getTestName(),randomVariant(testGroup.getVariantList()));
+            randomVariants.put(testGroup.getTestName(), randomVariant(testGroup.getVariantList()));
         }
         Json json = new Json();
         JsonNode jsonNode = json.toJson(randomVariants);
         return ok(jsonp(callback, jsonNode)).as("application/json");
     }
-    
+
     private static String randomVariant(List<Variant> variantList) {
         Double sumOfWeights = 0.0;
-        for(Variant variant : variantList){
-            sumOfWeights  += variant.getWeight();
+        for (Variant variant : variantList) {
+            sumOfWeights += variant.getWeight();
         }
         Double randomDouble = random.nextDouble() * sumOfWeights;
         for (Variant variant : variantList) {
